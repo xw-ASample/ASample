b042e36c2745895f18d3f66a1fc791935ea8a272
Dream <1627901679@qq.com>
2018-03-21 16:54:01 +0800

    添加测试方法

---
+++ test/ASample.BookShop.Service.Test/BookServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using ASample.BookShop.Dal.IRepository;
+using ASample.BookShop.Service.IService;
+using ASample.BookShop.Service.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASample.BookShop.Service.Test
+{
+    [TestClass]
+    public class BookServiceTest
+    {
+        //IBookRepository dal;
+        [TestMethod]
+        public void Select()
+        {
+
+        }
+    }
+}
---
+++ test/ASample.BookShop.Service.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ASample.BookShop.Service.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASample.BookShop.Service.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e58d88eb-eb4f-4b42-bd59-c3b5ab57f1c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
